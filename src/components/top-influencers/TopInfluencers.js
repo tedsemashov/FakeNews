b44f01e02f32ec4689ea1e695a302357f2e36583
Ted Semashov <tsema@softserveinc.com>
2019-08-20 18:50:26 +0300

    Implemented Influencers section

--- src/components/top-influencers/TopInfluencers.js
+++ src/components/top-influencers/TopInfluencers.js
@@ -5,12 +5,20 @@ import TopInfluencersSection from '../top-influencers/top-influencers-section/To
 
 class TopInfluencers extends Component {
 
-   componentDidMount() {
+   returnInfluencersSection = () => {
+      const topInfluencers = this.props.topInfluencers;
+      const influencersArr = Object.keys(topInfluencers).map((prop) => {
+         topInfluencers[prop].account = '@' + prop;
+         return topInfluencers[prop];
+      });
+      return influencersArr;
+   };
 
+   componentDidMount() {
    };
 
    render() {
-      const topInfluencers = this.props.topInfluencers;
+      const influencersArr =  this.returnInfluencersSection();
       return (
            <div className='topInfluencersContainer'>
               <div className='titleWrapperNews'>
@@ -19,9 +27,16 @@ class TopInfluencers extends Component {
               <div className='influencersWrapper'>
                  <div className='topInfluencersAccounts'>
                     {
-                       // topInfluencers.map(({user_name}) =>
-                       //      <TopInfluencersSection name={user_name}/>
-                       // )
+                       influencersArr.map(({user_name, user_profile_image_url,
+                                              account, user_followers_count, user_statuses_count}) =>
+                            <TopInfluencersSection
+                                 account={account}
+                                 name={user_name}
+                                 img={user_profile_image_url}
+                                 tweets={user_statuses_count}
+                                 followers={user_followers_count}
+                            />
+                       )
                     }
                  </div>
               </div>
--- src/components/top-influencers/top-influencers-section/TopInfluencersSection.js
+++ src/components/top-influencers/top-influencers-section/TopInfluencersSection.js
@@ -4,13 +4,32 @@ import './topInfluencersSection.css';
 class TopInfluencersSection extends Component {
 
    componentDidMount() {
+      console.log(this.props.account)
+   };
 
+   convertNumbers = (num) => {
+      return Math.abs(num) > 999 ? Math.sign(num)*((Math.abs(num)/1000).toFixed(1)) + 'K' : Math.sign(num)*Math.abs(num)
    };
 
    render() {
       return (
-           <div className='influencerAccountSection'>
-            <p>{this.props.name}</p>
+           <div className='accountSection'>
+              <div className='imageNameSection'>
+                 <img className='userImage' src={this.props.img}/>
+                 <div className='userInformWrapper'>
+                    <p className='userNickname'>{this.props.name}</p>
+                    <p className='userAccount'>{this.props.account}</p>
+                 </div>
+              </div>
+              <div className='buttonsSection'>
+                 <button className='btnTweets buttonsTweetFollowers'>
+                    {this.convertNumbers(this.props.tweets)} tweets
+                 </button>
+                 <button className='btnFollowers buttonsTweetFollowers'>
+                    {this.convertNumbers(this.props.followers)} followers
+                 </button>
+                 <button className='btnFakeNews btnFakeNewsGreen'>90% fake news</button>
+              </div>
            </div>
       );
    }
