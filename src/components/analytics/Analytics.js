2c75e515afdfaa1c5e9ac9d6ddca810bc9b09e77
htere <htere@softserveinc.com>
2019-08-21 18:57:29 +0300

    added DateRangePicker Component

--- src/components/analytics/Analytics.js
+++ src/components/analytics/Analytics.js
@@ -1,8 +1,8 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './analytics.css';
 import Header from '../header/Header';
-import Subheader from  '../subheader/Subheader';
-import TimeDropdown from  '../subheader/time-dropdown/TimeDropdown';
+import Subheader from '../subheader/Subheader';
+import TimeDropdown from '../subheader/time-dropdown/TimeDropdown';
 import Hashtags from '../hashtags/index';
 import TrollsActivity from '../trolls-activity/index';
 import TopNews from '../top-news/index';
@@ -13,64 +13,62 @@ import TopMentionedUser from '../top-mentioned-user/index';
 import Footer from '../footer/Footer';
 
 class Analytics extends Component {
-   state = {
-      timePeriod: false,
-      timePeriodValue: '15 min'
-   };
+  state = {
+    timePeriod: false,
+    timePeriodValue: '15 min'
+  };
 
-   componentDidMount() {
-      // this.props.getTwitterData();
-   };
+  componentDidMount() {
+    // this.props.getTwitterData();
+  }
 
-   toogleTimePeriod = () => {
-      this.setState(state => ({timePeriod: !state.timePeriod}))
-   };
+  toogleTimePeriod = () => {
+    this.setState(state => ({ timePeriod: !state.timePeriod }));
+  };
 
-   showTimePeriodDropdown = () => {
-      if(this.state.timePeriod) {
-         return (
-              <div className='timeDropdownWrapper'>
-                 <TimeDropdown/>
-              </div>
-         )
-      }
-   };
-
-   render() {
+  showTimePeriodDropdown = () => {
+    if (this.state.timePeriod) {
       return (
-           <div>
-              <Header/>
-              <Subheader onClick={this.toogleTimePeriod}
-                         timePeriodValue={this.state.timePeriodValue}
-              />
-              {this.showTimePeriodDropdown()}
-              <Hashtags/>
-              <TrollsActivity/>
-              <section className='topWrapper'>
-              <div className='topAllNewsWrapper'>
-                  <div className='topNewsWrapper'>
-                     <TopNews/>
-                  </div>
-                  <div className='topNewsWrapper'>
-                     <TopRetweetedNews/>
-                  </div>
-              </div>
-              <div className='influencersTrollsMentionedWrapper'>
-                 <TopInfluencers/>
-              </div>
-              <div className='influencersTrollsMentionedWrapper'>
-                  <TopTrolls/>
-              </div>
-              <div className='influencersTrollsMentionedWrapper'>
-                 <TopMentionedUser/>
-              </div>
-              </section>
-              <div className='footerWrapper'>
-                 <Footer/>
-              </div>
-           </div>
+        <div className="timeDropdownWrapper">
+          <TimeDropdown />
+        </div>
       );
-   }
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <Subheader onClick={this.toogleTimePeriod} timePeriodValue={this.state.timePeriodValue} />
+        {this.showTimePeriodDropdown()}
+        <Hashtags />
+        <TrollsActivity />
+        <section className="topWrapper">
+          <div className="topAllNewsWrapper">
+            <div className="topNewsWrapper">
+              <TopNews />
+            </div>
+            <div className="topNewsWrapper">
+              <TopRetweetedNews />
+            </div>
+          </div>
+          <div className="influencersTrollsMentionedWrapper">
+            <TopInfluencers />
+          </div>
+          <div className="influencersTrollsMentionedWrapper">
+            <TopTrolls />
+          </div>
+          <div className="influencersTrollsMentionedWrapper">
+            <TopMentionedUser />
+          </div>
+        </section>
+        <div className="footerWrapper">
+          <Footer />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Analytics;
