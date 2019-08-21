d7cbccd5474dbef48baa7412a2a94ee82fa10d06
Ted Semashov <tsema@softserveinc.com>
2019-08-21 17:27:45 +0300

    Finished Popup UI

---
+++ src/components/popup-top-accounts/popup-header-numbers/index.js
@@ -0,0 +1,14 @@
+import { connect } from "react-redux";
+import PopupHeaderNumbers from './PopupHeaderNumbers';
+
+const mapStateToProps = (state) => {
+   return {
+   }
+};
+
+const mapDispatchToProps = (dispatch) => {
+   return {
+   }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(PopupHeaderNumbers)
