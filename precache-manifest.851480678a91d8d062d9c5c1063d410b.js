41183bb472cba82f3e04ac3227e29e324a68326f
Ted Semashov <tsema@softserveinc.com>
2019-08-29 11:24:13 +0300

    Updates

---
+++ precache-manifest.851480678a91d8d062d9c5c1063d410b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43572e57d9ba561ca057d29c0263d313",
+    "url": "/tedsemashov/FakeNews/index.html"
+  },
+  {
+    "revision": "6ad7400b4679341bc652",
+    "url": "/tedsemashov/FakeNews/static/css/3.689ec195.chunk.css"
+  },
+  {
+    "revision": "dd4c264b8595d1d241a5",
+    "url": "/tedsemashov/FakeNews/static/css/main.ca909bc2.chunk.css"
+  },
+  {
+    "revision": "6ad7400b4679341bc652",
+    "url": "/tedsemashov/FakeNews/static/js/3.5fb2944b.chunk.js"
+  },
+  {
+    "revision": "5fb36f2a6869bae2dd07",
+    "url": "/tedsemashov/FakeNews/static/js/4.638ca290.chunk.js"
+  },
+  {
+    "revision": "8f1d222d209aa8f3b9f7",
+    "url": "/tedsemashov/FakeNews/static/js/5.7c62b074.chunk.js"
+  },
+  {
+    "revision": "41f5dfee8e54d4141769",
+    "url": "/tedsemashov/FakeNews/static/js/6.d007a2b2.chunk.js"
+  },
+  {
+    "revision": "dd4c264b8595d1d241a5",
+    "url": "/tedsemashov/FakeNews/static/js/main.4b9be352.chunk.js"
+  },
+  {
+    "revision": "bdfa9d579b9e3c087b5b",
+    "url": "/tedsemashov/FakeNews/static/js/runtime~main.38e8c4bc.js"
+  },
+  {
+    "revision": "8a865e6ce9718e11ed5f",
+    "url": "/tedsemashov/FakeNews/static/js/xlsx.bf4962bd.chunk.js"
+  },
+  {
+    "revision": "9f8c00f90c2044feca0ee1d3cc722185",
+    "url": "/tedsemashov/FakeNews/static/media/Steven_Hallam-slide.9f8c00f9.jpg"
+  },
+  {
+    "revision": "4b59bf9c760c7de410b84ee90176ac03",
+    "url": "/tedsemashov/FakeNews/static/media/logo@3x.4b59bf9c.png"
+  },
+  {
+    "revision": "58bbb1650cea94bf6990517f26adf4b9",
+    "url": "/tedsemashov/FakeNews/static/media/logo@3x.58bbb165.png"
+  }
+]);
---
+++ static/js/xlsx.bf4962bd.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{251:function(n,o){},262:function(n,o){},263:function(n,o){}}]);
+//# sourceMappingURL=xlsx.bf4962bd.chunk.js.map
