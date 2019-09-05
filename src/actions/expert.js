3222c9f93f4447dedb4dbbadd8a0ac470fb69b57
ShevchukTania <taniabeley@ukr.net>
2019-09-05 12:50:17 +0300

    fix

--- src/actions/expert.js
+++ src/actions/expert.js
@@ -10,7 +10,6 @@ export function getExpertsData() {
       expertsConstants.EXPERT_BODY_POST,
       {headers: {'Content-Type': 'application/json'}}
     ).then((response) => {
-      console.log(response);
       const data = response.data;
       dispatch(setExpertsData(response.data));
       dispatch(setLoadingState(true));
