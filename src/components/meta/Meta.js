838150729e69eec8b6ee7ab941ca60080af545c2
ShevchukTania <taniabeley@ukr.net>
2019-09-06 18:26:06 +0300

    setDocumentTitle function fix

--- src/components/meta/Meta.js
+++ src/components/meta/Meta.js
@@ -2,11 +2,12 @@ import React from 'react';
 import MetaTags from 'react-meta-tags';
 import _ from 'lodash';
 
-export function setDocumentTitle({title}) {
+export function setDocumentTitle(title) {
   const metaTitle = _.join(_.compact(['Fake News', title]), ' | ');
 
   return(
     <MetaTags>
+    debugger;
       <title>{metaTitle}</title>
     </MetaTags>
   );
