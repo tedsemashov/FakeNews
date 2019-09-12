b9b6306d46f666d3a7d0800e157621ee4e8c3c24
Roman Solomud <romaslmd@gmail.com>
2019-09-13 00:19:42 +0300

    update top users lists on expert page

--- src/components/top-mentioned-users-re-tweets/TopMentionedUsersReTweets.js
+++ src/components/top-mentioned-users-re-tweets/TopMentionedUsersReTweets.js
@@ -5,13 +5,32 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
+import Spinner from "react-bootstrap/Spinner";
+import Slider from "react-slick";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronUp, faChevronDown } from "@fortawesome/free-solid-svg-icons";
 
 import SectionTitle from "../section-title/SectionTitle";
 import { userAvatar } from "../../utils/avatar";
-import Spinner from "react-bootstrap/Spinner";
 
 import "./top-mentioned-users-re-tweets.css";
 import checkIcon from "./../../images/tip_icon.svg";
+
+export function NextArrow({ currentSlide, slideCount, ...props }) {
+  return(
+    <div {...props}>
+      <FontAwesomeIcon icon={faChevronDown} />
+    </div>
+  );
+}
+
+export function PrevArrow({ currentSlide, slideCount, ...props }) {
+  return(
+    <div {...props}>
+      <FontAwesomeIcon icon={faChevronUp} />
+    </div>
+  );
+};
 
 export default class TopMentionedUsersReTweets extends React.Component {
   spinner = (
@@ -32,14 +51,8 @@ export default class TopMentionedUsersReTweets extends React.Component {
 
     this.state = { user: null };
 
-    this.onMarkFake = this.onMarkFake.bind(this);
-
     this.renderUser = this.renderUser.bind(this);
     this.renderTweets = this.renderTweets.bind(this);
-  }
-
-  onMarkFake(user) {
-    console.log("onMarkFake", user);
   }
 
   renderButton(user) {
@@ -65,22 +78,33 @@ export default class TopMentionedUsersReTweets extends React.Component {
   renderUser(userData, user) {
     const isProcessing = _.includes(this.props.processing, user);
     const onSelect = () => this.setState({ user });
+    const selectedUser = this.state.user ||  _.keys(this.props.users)[0];
+    const isSelected = selectedUser === user;
+    const className = classNames("user-row", { isSelected });
+    const index = `tmurt-${user}-${isProcessing}`;
 
     return(
-      <Row className="m-0 user-row" key={`${user}-${isProcessing}`} >
+      <div data-index={index} className={className} key={index} >
         {isProcessing && this.spinner}
 
-        <Col className="pl-0 user-selection" sm={9} onClick={onSelect}>
-          <Row>
-            <Col className="" sm={3}>{userAvatar({...userData, nickname: user})}</Col>
-            <Col className="user-details" sm={6}>
-              <h5 className="user-name"> </h5>
-              <span className="user-nickname">@{user}</span>
-            </Col>
-          </Row>
-        </Col>
-        <Col className="text-right pr-0" sm={3}>{this.renderButton(user)}</Col>
-      </Row>
+        <Row>
+          <Col sm={9}>
+            <div className="user-selection" onClick={onSelect}>
+              <Row>
+                <Col sm={3}>{userAvatar({...userData, nickname: user})}</Col>
+                <Col className="user-details" sm={9}>
+                  <h5 className="user-name"> </h5>
+                  <span className="user-nickname">@{user}</span>
+                </Col>
+              </Row>
+            </div>
+          </Col>
+
+          <Col sm={3}>
+            {this.renderButton(user)}
+          </Col>
+        </Row>
+      </div>
     );
   }
 
@@ -103,11 +127,27 @@ export default class TopMentionedUsersReTweets extends React.Component {
                 <Col sm={{ offset: 9, span: 3}}>Mark fake user</Col>
               </Row>
 
-              <div className="users-container">{usersList}</div>
+              <div className="users-container-wrapper">
+                {
+                  !_.isEmpty(usersList) &&
+                  <Slider
+                    ref={(e) => this.slider = e}
+                    infinite={false}
+                    slidesToShow={4}
+                    slidesToScroll={1}
+                    nextArrow={<NextArrow />}
+                    prevArrow={<PrevArrow />}
+                    vertical
+                    verticalSwiping
+                    >
+                    {usersList}
+                  </Slider>
+                }
+              </div>
             </Col>
             <Col className="tweets-layout" sm={6}>
               <div className="tweets-list">
-                {tweetsList}
+                {_.isEmpty(tweetsList) ? "No data" : tweetsList}
               </div>
             </Col>
           </Row>
