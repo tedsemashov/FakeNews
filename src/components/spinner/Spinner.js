fc3677f0d5ea7820f1c76e7b7ee05d9aa20837a3
ShevchukTania <taniabeley@ukr.net>
2019-09-18 19:13:20 +0300

    fix

--- src/components/spinner/Spinner.js
+++ src/components/spinner/Spinner.js
@@ -4,7 +4,7 @@ import classNames from "classnames";
 import "./spinner.css";
 
 export default function Spinner(props) {
-  const className = classNames("spinnerWrapper spinner-layout", props.size);
+  const className = classNames("spinner-layout", props.size);
   return(
     <div className={className}>
       <img src={require('../../images/spiner.gif')} alt="spinner" />
