5bf9b80b88f0fd74e346dd6d39ae0af17a27b5a0
Roman Solomud <romaslmd@gmail.com>
2019-10-22 10:59:22 +0300

    restore keyword action

--- src/components/hashtags/index.js
+++ src/components/hashtags/index.js
@@ -3,13 +3,14 @@ import _ from "lodash";
 
 import Hashtags from "./Hashtags";
 
-import {getTwitterData, setLoadingState} from "../../actions";
+import {getTwitterData, setLoadingState, setKeywordData} from "../../actions";
 
 const mapStateToProps = (state) => _.pick(state, ["keyword", "hashtags", "timePeriod", "tweets_count_ts"]);
 const mapDispatchToProps = (dispatch) => {
   return {
     onTagClick: (keyword, timePeriod) => {
       dispatch(setLoadingState(false));
+      dispatch(setKeywordData(keyword));
       dispatch(getTwitterData({ keyword, timePeriod }));
     }
   };
